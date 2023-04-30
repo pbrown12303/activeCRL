807aab2106d00dce6ee24bd6065e32b59495c7d9
pbrown12303 <pbrown12303@gmail.com>
2023-04-30 15:55:27 -0400

    DiagramElement creation working for all diagram elements

--- crleditorfynegui/FyneDiagramManager.go
+++ crleditorfynegui/FyneDiagramManager.go
@@ -2,6 +2,7 @@ package crleditorfynegui
 
 import (
 	"image/color"
+	"reflect"
 
 	"fyne.io/x/fyne/widget/diagramwidget"
 
@@ -16,19 +17,23 @@ import (
 	"github.com/pbrown12303/activeCRL/images"
 )
 
+// FyneDiagramManager manages the relationship between the fyne DiagramWidgets and the
+// underlying CRL model. It is a component of the  FyneGUI
 type FyneDiagramManager struct {
-	fyneGUI         *FyneGUI
-	diagramArea     *fyne.Container
-	diagramTabs     map[string]*container.TabItem
-	toolbar         *fyne.Container
-	toolButtons     map[string]*widget.Button
-	tabArea         *container.DocTabs
-	cursorTool      *widget.Button
-	elementTool     *widget.Button
-	LiteralTool     *widget.Button
-	diagramObserver *diagramObserver
+	fyneGUI                *FyneGUI
+	diagramArea            *fyne.Container
+	diagramTabs            map[string]*container.TabItem
+	toolbar                *fyne.Container
+	toolButtons            map[string]*widget.Button
+	tabArea                *container.DocTabs
+	cursorTool             *widget.Button
+	elementTool            *widget.Button
+	LiteralTool            *widget.Button
+	diagramObserver        *diagramObserver
+	diagramElementObserver *diagramElementObserver
 }
 
+// NewFyneDiagramManager creates a diagram manager and associates it with the FyneGUI
 func NewFyneDiagramManager(fyneGUI *FyneGUI) *FyneDiagramManager {
 	var dm FyneDiagramManager
 	dm.createToolbar()
@@ -37,6 +42,7 @@ func NewFyneDiagramManager(fyneGUI *FyneGUI) *FyneDiagramManager {
 	dm.tabArea.OnClosed = diagramClosed
 	dm.diagramArea = container.NewBorder(nil, nil, dm.toolbar, nil, dm.tabArea)
 	dm.diagramObserver = newDiagramObserver(&dm)
+	dm.diagramElementObserver = newDiagramElementObserver(&dm)
 	return &dm
 }
 
@@ -103,69 +109,129 @@ func (dm *FyneDiagramManager) displayDiagram(diagram core.Element, trans *core.T
 	diagramID := diagram.GetConceptID(trans)
 	tabItem := dm.diagramTabs[diagramID]
 	if tabItem == nil {
-		tabItem = container.NewTabItem(diagram.GetLabel(trans), diagramwidget.NewDiagramWidget(diagramID))
+		scrollingContainer := container.NewScroll(diagramwidget.NewDiagramWidget(diagramID))
+		tabItem = container.NewTabItem(diagram.GetLabel(trans), scrollingContainer)
 		dm.diagramTabs[diagramID] = tabItem
 		dm.tabArea.Append(tabItem)
 		diagram.Register(dm.diagramObserver)
-		dm.refreshDiagram(diagram, trans)
+		dm.populateDiagram(diagram, trans)
 	}
 	return nil
+}
+
+func (dm *FyneDiagramManager) getDiagramWidget(diagramID string) *diagramwidget.DiagramWidget {
+	tabItem := dm.diagramTabs[diagramID]
+	diagramWidget := tabItem.Content.(*container.Scroll).Content.(*diagramwidget.DiagramWidget)
+	return diagramWidget
 }
 
 func (dm *FyneDiagramManager) GetDrawingArea() *fyne.Container {
 	return dm.diagramArea
 }
 
-// refreshDiagram resends all diagram elements to the browser
-func (dm *FyneDiagramManager) refreshDiagram(diagram core.Element, trans *core.Transaction) error {
-	tabItem := dm.diagramTabs[diagram.GetConceptID(trans)]
-	diagramWidget := tabItem.Content.(*diagramwidget.DiagramWidget)
+// populateDiagram adds all elements to the diagram
+func (dm *FyneDiagramManager) populateDiagram(diagram core.Element, trans *core.Transaction) error {
+	diagramWidget := dm.getDiagramWidget(diagram.GetConceptID(trans))
 	nodes := diagram.GetOwnedConceptsRefinedFromURI(crldiagramdomain.CrlDiagramNodeURI, trans)
 	for _, node := range nodes {
 		// Get the node ID
-		nodeID := node.GetConceptID(trans)
 		// Get the icon
+		// Get the abstraction string
+		// Build the node content
+		// Display the text
+		// Now create the node itself
+		dm.addNodeToDiagram(node, trans, diagramWidget)
+	}
+	links := diagram.GetOwnedConceptsRefinedFromURI(crldiagramdomain.CrlDiagramLinkURI, trans)
+	for _, link := range links {
+		linkID := link.GetConceptID(trans)
+		diagramLink := diagramWidget.Links[linkID]
+		if diagramLink == nil {
+			dm.addLinkToDiagram(link, trans, diagramWidget)
+		}
+	}
+	diagramWidget.Refresh()
+	return nil
+}
+
+func (dm *FyneDiagramManager) addElementToDiagram(element core.Element, trans *core.Transaction, diagramWidget *diagramwidget.DiagramWidget) {
+	if element.IsRefinementOfURI(crldiagramdomain.CrlDiagramNodeURI, trans) {
+		dm.addNodeToDiagram(element, trans, diagramWidget)
+	} else if element.IsRefinementOfURI(crldiagramdomain.CrlDiagramLinkURI, trans) {
+		dm.addLinkToDiagram(element, trans, diagramWidget)
+	}
+}
+
+func (dm *FyneDiagramManager) addLinkToDiagram(link core.Element, trans *core.Transaction, diagramWidget *diagramwidget.DiagramWidget) {
+	crlDiagramSource := crldiagramdomain.GetLinkSource(link, trans)
+	if crlDiagramSource == nil {
+		// Register for changes so that when sufficient information is present we can add it to the diagram
+		link.Register(dm.diagramElementObserver)
+		return
+	}
+	fyneSource := diagramWidget.GetDiagramElement(crlDiagramSource.GetConceptID(trans))
+	fyneSourcePad := fyneSource.GetDefaultConnectionPad()
+	crlDiagramTarget := crldiagramdomain.GetLinkTarget(link, trans)
+	if crlDiagramTarget == nil {
+		// Register for changes so that when sufficient information is present we can add it to the diagram
+		link.Register(dm.diagramElementObserver)
+		return
+	}
+	fyneTarget := diagramWidget.GetDiagramElement(crlDiagramTarget.GetConceptID(trans))
+	fyneTargetPad := fyneTarget.GetDefaultConnectionPad()
+	diagramLink := diagramwidget.NewDiagramLink(diagramWidget, fyneSourcePad, fyneTargetPad, link.GetConceptID(trans))
+	diagramLink.AddMidpointAnchoredText("displayLabel", crldiagramdomain.GetDisplayLabel(link, trans))
+	grey := color.RGBA{153, 153, 153, 255}
+	if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramReferenceLinkURI, trans) {
+		diagramLink.AddTargetDecoration(createReferenceArrowhead())
+		diagramLink.AddSourceDecoration(createDiamond())
+	} else if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramAbstractPointerURI, trans) {
+		diagramLink.AddSourceDecoration(createRefinementTriangle())
+		diagramLink.LinkColor = grey
+	} else if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramElementPointerURI, trans) {
+		diagramLink.AddTargetDecoration(createReferenceArrowhead())
+		diagramLink.LinkColor = grey
+	} else if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramOwnerPointerURI, trans) {
+		diagramLink.AddTargetDecoration(createDiamond())
+		diagramLink.LinkColor = grey
+	} else if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramRefinedPointerURI, trans) {
+		diagramLink.AddSourceDecoration(createMirrorRefinementTriangle())
+		diagramLink.LinkColor = grey
+	} else if link.IsRefinementOfURI(crldiagramdomain.CrlDiagramRefinementLinkURI, trans) {
+		diagramLink.AddMidpointDecoration(createRefinementTriangle())
+	}
+	link.Register(dm.diagramElementObserver)
+}
+
+func (dm *FyneDiagramManager) addNodeToDiagram(node core.Element, trans *core.Transaction, diagramWidget *diagramwidget.DiagramWidget) {
+	nodeID := node.GetConceptID(trans)
+	diagramNode := diagramWidget.Nodes[nodeID]
+	if diagramNode == nil {
+
 		modelElement := crldiagramdomain.GetReferencedModelElement(node, trans)
 		nodeIcon := widget.NewIcon(getIconResource(modelElement, trans))
-		// Get the abstraction string
+
 		abstractionString := crldiagramdomain.GetAbstractionDisplayLabel(node, trans)
 		abstractionText := canvas.NewText(abstractionString, color.Black)
 		abstractionText.TextSize = diagramWidget.DiagramTheme.Size(theme.SizeNameCaptionText)
 		abstractionText.TextStyle = fyne.TextStyle{Bold: false, Italic: true, Monospace: false, Symbol: false, TabWidth: 4}
-		// Build the node content
+
 		hBox := container.NewHBox(nodeIcon, abstractionText)
 		nodeLabel := crldiagramdomain.GetDisplayLabel(node, trans)
 		entryWidget := widget.NewEntry()
 		entryWidget.SetText(nodeLabel)
 		entryWidget.Wrapping = fyne.TextWrapOff
-		entryWidget.Refresh() // Display the text
+		entryWidget.Refresh()
 		nodeContainer := container.NewVBox(hBox, entryWidget)
-		// Now create the node itself
-		diagramNode := diagramwidget.NewDiagramNode(diagramWidget, nodeContainer, nodeID)
+
+		diagramNode = diagramwidget.NewDiagramNode(diagramWidget, nodeContainer, nodeID)
 		x := crldiagramdomain.GetNodeX(node, trans)
 		y := crldiagramdomain.GetNodeY(node, trans)
 		fynePosition := fyne.NewPos(float32(x), float32(y))
 		diagramNode.Move(fynePosition)
 		diagramNode.Refresh()
+		node.Register(dm.diagramElementObserver)
 	}
-	// links := diagram.GetOwnedConceptsRefinedFromURI(crldiagramdomain.CrlDiagramLinkURI, hl)
-	// for _, link := range links {
-	// additionalParameters := getLinkAdditionalParameters(link, hl)
-	// conceptState, err2 := core.NewConceptState(link)
-	// if err2 != nil {
-	// 	return errors.Wrap(err2, "diagramManager.refreshDiagram failed")
-	// }
-	// link.Register(dmPtr.elementManager)
-	// notificationResponse, err := BrowserGUISingleton.SendNotification("AddDiagramLink", link.GetConceptID(hl), conceptState, additionalParameters)
-	// if err != nil {
-	// 	return errors.Wrap(err, "diagramManager.refreshDiagram failed")
-	// }
-	// if notificationResponse.Result != 0 {
-	// 	return errors.New(notificationResponse.ErrorMessage)
-	// }
-	// }
-	diagramWidget.Refresh()
-	return nil
 }
 
 func diagramClosed(tabItem *container.TabItem) {
@@ -190,8 +256,8 @@ func newDiagramObserver(dm *FyneDiagramManager) *diagramObserver {
 	return &do
 }
 
-// updateDiagram is the callback for changes to the core diagram
-func (do *diagramObserver) Update(notification *core.ChangeNotification, heldLocks *core.Transaction) error {
+// Update is the callback for changes to the core diagram
+func (do *diagramObserver) Update(notification *core.ChangeNotification, trans *core.Transaction) error {
 	tabItem := do.diagramManager.diagramTabs[notification.GetReportingElementID()]
 	if tabItem == nil {
 		// the diagram is not being displayed
@@ -205,6 +271,103 @@ func (do *diagramObserver) Update(notification *core.ChangeNotification, heldLoc
 			tabItem.Text = afterStateLabel
 			do.diagramManager.tabArea.Refresh()
 		}
+	case core.OwnedConceptChanged:
+		underlyingChange := notification.GetUnderlyingChange()
+		beforeState := underlyingChange.GetBeforeConceptState()
+		afterState := underlyingChange.GetAfterConceptState()
+		diagramWidget := do.diagramManager.getDiagramWidget(notification.GetReportingElementID())
+		switch underlyingChange.GetNatureOfChange() {
+		case core.OwningConceptChanged:
+			if beforeState.OwningConceptID == "" && afterState.OwningConceptID != "" {
+				// the element has been added
+				uOfD := trans.GetUniverseOfDiscourse()
+				element := uOfD.GetElement(afterState.ConceptID)
+				do.diagramManager.addElementToDiagram(element, trans, diagramWidget)
+			} else {
+				// the element has been removed
+			}
+
+		}
 	}
 	return nil
+}
+
+type diagramElementObserver struct {
+	diagramManager *FyneDiagramManager
+}
+
+func newDiagramElementObserver(dm *FyneDiagramManager) *diagramElementObserver {
+	deo := diagramElementObserver{diagramManager: dm}
+	return &deo
+}
+
+// Update is the callback for changes to the core diagram element
+func (deo *diagramElementObserver) Update(notification *core.ChangeNotification, trans *core.Transaction) error {
+	diagramID := notification.GetReportingElementState().OwningConceptID
+	tabItem := deo.diagramManager.diagramTabs[diagramID]
+	if tabItem == nil {
+		// the diagram is not being displayed
+		return nil
+	}
+	diagramWidget := deo.diagramManager.getDiagramWidget(notification.GetReportingElementState().OwningConceptID)
+	elementID := notification.GetReportingElementID()
+	crlDiagramElement := trans.GetUniverseOfDiscourse().GetElement(elementID)
+	fyneDiagramElement := diagramWidget.GetDiagramElement(elementID)
+	if reflect.ValueOf(fyneDiagramElement).IsNil() {
+		deo.diagramManager.addElementToDiagram(crlDiagramElement, trans, diagramWidget)
+	}
+	return nil
+}
+
+var referenceArrowHeadPoints []fyne.Position = []fyne.Position{
+	{X: 0, Y: 0},
+	{X: 8, Y: 5},
+	{X: 8, Y: -5},
+}
+
+func createReferenceArrowhead() *diagramwidget.Polygon {
+	polygon := diagramwidget.NewPolygon(referenceArrowHeadPoints)
+	polygon.SetSolid(true)
+	polygon.SetClosed(true)
+	return polygon
+}
+
+var diamondPoints []fyne.Position = []fyne.Position{
+	{X: 0, Y: 0},
+	{X: 8, Y: 4},
+	{X: 16, Y: 0},
+	{X: 8, Y: -4},
+}
+
+func createDiamond() *diagramwidget.Polygon {
+	polygon := diagramwidget.NewPolygon(diamondPoints)
+	polygon.SetSolid(true)
+	polygon.SetClosed(true)
+	return polygon
+}
+
+var refinementTrianglePoints []fyne.Position = []fyne.Position{
+	{X: 0, Y: 8},
+	{X: 16, Y: 0},
+	{X: 0, Y: -8},
+}
+
+func createRefinementTriangle() *diagramwidget.Polygon {
+	polygon := diagramwidget.NewPolygon(refinementTrianglePoints)
+	polygon.SetSolid(false)
+	polygon.SetClosed(true)
+	return polygon
+}
+
+var mirrorRefinementTrianglePoints []fyne.Position = []fyne.Position{
+	{X: 0, Y: 0},
+	{X: 16, Y: 8},
+	{X: 16, Y: -8},
+}
+
+func createMirrorRefinementTriangle() *diagramwidget.Polygon {
+	polygon := diagramwidget.NewPolygon(mirrorRefinementTrianglePoints)
+	polygon.SetSolid(false)
+	polygon.SetClosed(true)
+	return polygon
 }
--- go.mod
+++ go.mod
@@ -1,29 +1,20 @@
 module github.com/pbrown12303/activeCRL
 
-go 1.15
+go 1.14
 
 require (
-	fyne.io/fyne/v2 v2.3.0
-	fyne.io/systray v1.10.1-0.20230207085535-4a244dbb9d03 // indirect
-	fyne.io/x/fyne v0.0.0-00010101000000-000000000000
-	github.com/TheTitanrain/w32 v0.0.0-20200114052255-2654d97dbd3d // indirect
+	fyne.io/fyne/v2 v2.3.3
+	fyne.io/x/fyne v0.0.0-20230411205836-1ca0ead20763
 	github.com/awalterschulze/gographviz v2.0.3+incompatible
 	github.com/deckarep/golang-set v1.8.0
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/fyne-io/image v0.0.0-20221020213044-f609c6a24345 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/ginkgo/v2 v2.8.0
-	github.com/onsi/gomega v1.26.0
+	github.com/onsi/ginkgo/v2 v2.9.2
+	github.com/onsi/gomega v1.27.6
 	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
 	github.com/sclevine/agouti v3.0.0+incompatible
 	github.com/sqweek/dialog v0.0.0-20220809060634-e981b270ebbf
-	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
-	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
-	golang.org/x/image v0.5.0
-	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd
 	nhooyr.io/websocket v1.8.7
 )
 
